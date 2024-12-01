d6ecfcb5cb41960e7cfedae1e9e10c0837f4e5b1
Krushovice <kickstar69@yandex.ru>
2024-12-01 14:20:43 +1000

    create payment model

---
+++ app/core/models/payment.py
@@ -0,0 +1,27 @@
+import datetime
+
+from sqlalchemy import Integer, Date, ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+
+
+class Payment(Base):
+    __tablename__ = "payments"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
+    created_at: Mapped[datetime] = mapped_column(
+        Date,
+        default=datetime.datetime.today().strftime("%Y-%m-%d"),
+        server_default=func.current_date(),
+    )
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"),
+    )
+    # Добавляем отношение многие к одному с моделью User
+    user: Mapped["User"] = relationship(
+        back_populates="payments",
+        lazy="selectin",
+    )
