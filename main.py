66bf39561e7d32c3402e8b7963338505659c6c72
Krushovice <kickstar69@yandex.ru>
2024-12-01 12:55:28 +1000

    create main script

--- main.py
+++ main.py
@@ -1,14 +1,62 @@
 import asyncio
 
+
+from aiogram import Bot, Dispatcher
+
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
 from app.core.logging import setup_logger
 
+from app.core.config import settings
 
-setup_logger(__name__)
+from app.commands import router as commands_router
+
+logger = setup_logger(__name__)
+
 
 async def main():
-    pass
+    try:
+        dp = Dispatcher()
+        bot = Bot(
+            token=settings.bot_token,
+            default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+        )
+
+        dp.include_router(commands_router)
+        await dp.start_polling(bot)
+
+    except Exception as e:
+        logger.error(e)
+
+
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
 
+# webhook example
 
+# from aiohttp import web
+# from aiogram import Bot, Dispatcher
+#
+# API_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+# WEBHOOK_PATH = "/webhook"
+# WEBHOOK_URL = f"https://your-domain.com{WEBHOOK_PATH}"
+#
+# bot = Bot(token=API_TOKEN)
+# dp = Dispatcher()
+#
+# # Обработка входящих апдейтов
+# async def handle_update(request: web.Request):
+#     update = await request.json()
+#     await bot.process_update(update)
+#     return web.Response()
+#
+# app = web.Application()
+# app.router.add_post(WEBHOOK_PATH, handle_update)
+
+# async def set_webhook():
+#     await bot.set_webhook(WEBHOOK_URL)
+# #
+# if __name__ == "__main__":
+#     web.run_app(app, host="0.0.0.0", port=8443)
