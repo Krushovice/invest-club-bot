a0d9d5ca081965ca9560aa3ffa4e8c6d32a1bd5c
Krushovice <kickstar69@yandex.ru>
2024-12-07 20:50:50 +1000

    create first chat callback handler

--- app/keyboards/__init__.py
+++ app/keyboards/__init__.py
@@ -2,7 +2,12 @@ __all__ = (
     "PayActions",
     "PayCbData",
     "pay_kb",
+    "ChatCbData",
+    "ChatActions",
+    "build_chat_kb",
 )
 
 
 from .paymet_kb import pay_kb, PayActions, PayCbData
+
+from .chat_kb import build_chat_kb, ChatActions, ChatCbData
--- app/routers/channel_handlers/admin_handlers.py
+++ app/routers/channel_handlers/admin_handlers.py
@@ -1,14 +1,26 @@
-from aiogram import Bot
+from aiogram import Bot, Router, F
 from aiogram.types import CallbackQuery
 
 from app.core.config import settings
 
+# from app.core.models import User
 
-async def handle_new_member(call: CallbackQuery, name: str):
+from app.keyboards.chat_kb import ChatCbData, ChatActions, build_chat_kb
+
+router = Router(name=__name__)
+
+
+@router.callback_query(ChatCbData.filter(F.action == ChatActions.join))
+async def handle_new_member(call: CallbackQuery, bot: Bot):
     msg = (
-        f"Приветствую, {name}!"
+        f"Приветствую!"
         f"Добро пожаловать в узкий круг инвесторов🤝 "
         f"Здесь вы найдёте всю актуальную информацию по сделкам и стратегии следования ✍ "
     )
 
-    # await call.message.new_chat_members(chat_id=settings.main.admin_id, text=msg)
+    # if user.subscription:
+    await bot.approve_chat_join_request(
+        chat_id=int(settings.main.channel_id),
+        user_id=1130398207,
+    )
+    await call.message.answer(msg)
