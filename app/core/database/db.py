0468b6458a55079dc1b6ebefc05cfe9a73cd1d64
Krushovice <kickstar69@yandex.ru>
2024-12-07 23:00:04 +1000

    create db connection decorator

---
+++ app/core/database/db.py
@@ -0,0 +1,16 @@
+from .db_helper import db_helper
+
+
+def connection(method):
+    async def wrapper(*args, **kwargs):
+        async with db_helper.session_factory() as session:
+            try:
+                # Явно не открываем транзакции, так как они уже есть в контексте
+                return await method(*args, session=session, **kwargs)
+            except Exception as e:
+                await session.rollback()  # Откатываем сессию при ошибке
+                raise e  # Поднимаем исключение дальше
+            finally:
+                await session.close()  # Закрываем сессию
+
+    return wrapper
