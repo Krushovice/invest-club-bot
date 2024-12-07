5da0ee3bf5cccf2df904883107c5b24badd1b02e
Krushovice <kickstar69@yandex.ru>
2024-12-07 20:51:24 +1000

    add create invite link if payment is success

--- app/routers/callback_handlers/pay_cb_handlers.py
+++ app/routers/callback_handlers/pay_cb_handlers.py
@@ -9,8 +9,6 @@ from app.routers.commands.main_commands import command_start_handler
 
 from app.routers.channel_handlers import handle_new_member
 
-from app.payment import payment_manager
-from app.core.config import settings
 
 router = Router(name=__name__)
 
@@ -20,54 +18,3 @@ async def handle_back_button(call: CallbackQuery):
     await call.answer()
     await command_start_handler(message=call.message)
     await handle_new_member(call, name=call.message.from_user.first_name)
-
-
-@router.callback_query(PayCbData.filter(F.action == PayActions.success))
-async def handle_success(call: CallbackQuery):
-    await call.answer()
-    status = await payment_manager.get_payment_status(call.dat.payment_id)
-    if status == "CONFIRMED":
-        await handle_new_member(call, name=call.message.from_user.first_name)
-
-
-# # Хэндлер нажатия кнопки "Оплатить"
-# @router.callback_query(PayCbData.filter(F.action == PayActions.pay))
-# async def pay_callback(call: CallbackQuery):
-#     await call.answer()
-#
-#     user_id = call.from_user.id
-#     payment_data = {
-#         "user_id": user_id,
-#         "amount": 10000,  # сумма платежа
-#         "description": "Оплата доступа",
-#         "success_url": f"https://latiy-trading.ru/success/{user_id}",  # URL после успешной оплаты
-#         "webhook_url": f"{settings.web.base_url}/{settings.bot.token}",
-#     }
-#
-#     async with call.session.post("", json=payment_data) as response:
-#         if response.status == 200:
-#             data = await response.json()
-#             payment_link = data.get("payment_link")
-#             await call.message.answer(f"Для оплаты перейдите по ссылке: {payment_link}")
-#         else:
-#             await call.message.answer("Ошибка при создании платежа. Попробуйте позже.")
-#
-#
-# # Обработка webhook'а от платежной системы
-# async def payment_webhook(request: web.Request):
-#     try:
-#         data = await request.json()
-#
-#         user_id = data["user_id"]
-#         payment_status = data["status"]
-#
-#         if payment_status == "success":
-#             await bot.send_message(
-#                 user_id,
-#                 "Оплата успешно завершена! Вот ваша ссылка: https://example.com/resource",
-#             )
-#
-#         return web.Response(status=200)
-#     except Exception as e:
-#         logging.error(f"Ошибка при обработке webhook: {e}")
-#         return web.Response(status=500)
--- app/utils/tools.py
+++ app/utils/tools.py
@@ -1,10 +1,13 @@
-import json
 import logging
 
 from aiogram import Bot
 from aiogram.types import FSInputFile
 
 from aiohttp import web
+
+from app.core.config import settings
+
+from app.keyboards import build_chat_kb
 
 image_path = "app/utils/images/image3.JPEG"
 
@@ -28,16 +31,24 @@ async def handle_payment_notification(request, bot: Bot):
             return web.json_response({"status": "missing required fields"}, status=400)
 
         status = data["Status"]
+        # если платеж успешен и его еще нет в бд
         if status == "CONFIRMED":
             logging.info(f"Платеж подтвержден: {data}")
 
             # payment = save_user_payment()
             # tg_id = payment.user.tg_id
             # Отправка сообщения пользователю
+
+            chat_link = await bot.create_chat_invite_link(
+                chat_id=int(settings.main.channel_id),
+                name=f"Инвайт ссылка юзера: {1130398207}",
+                member_limit=1,
+            )
             await bot.send_photo(
-                chat_id=tg_id,
+                chat_id=1130398207,
                 photo=FSInputFile(image_path),
-                caption="Ваш платеж успешно подтвержден!",
+                caption="Ваш платеж успешно подтвержден! Ссылка на закрытый чат ниже",
+                reply_markup=build_chat_kb(invite_link=chat_link.invite_link),
             )
             return web.json_response({"status": "ok"})
         else:
