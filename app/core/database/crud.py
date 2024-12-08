ebe331a7760954e9af64f7620bfed817fa70c555
Krushovice <kickstar69@yandex.ru>
2024-12-08 12:40:50 +1000

    add update user in crud

--- app/core/database/crud.py
+++ app/core/database/crud.py
@@ -1,10 +1,12 @@
+from typing import List
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .db import connection
 
 from .base_classes import UserOrm, PaymentOrm
 
-from .shemas import UserSchema, PaymentSchema
+from .shemas import UserSchema, PaymentSchema, UserUpdateSchema
 from app.core.models import User, Payment
 
 
@@ -20,6 +22,24 @@ class UserCRUD:
     @connection
     async def get_user_by_tg_id(tg_id: int, session: AsyncSession) -> User:
         user = await UserOrm.find_one_or_none(tg_id=tg_id, session=session)
+        return user
+
+    @staticmethod
+    @connection
+    async def get_users(session: AsyncSession) -> List[User]:
+        users = await UserOrm.find_all(session=session)
+        return users
+
+    @staticmethod
+    @connection
+    async def update_user(
+        user_id: int, user: UserUpdateSchema, session: AsyncSession
+    ) -> User:
+        user = await UserOrm.update(
+            session=session,
+            pk=user_id,
+            **user.model_dump(exclude_none=True),
+        )
         return user
 
     @staticmethod
@@ -40,10 +60,10 @@ class PaymentCRUD:
 
     @staticmethod
     @connection
-    async def get_payment_by_tg_id(session: AsyncSession, tg_id: int) -> Payment:
+    async def get_payment_by_pay_id(session: AsyncSession, payment_id: int) -> Payment:
         payment = await PaymentOrm.find_one_or_none(
             session=session,
-            tg_id=tg_id,
+            pay_id=payment_id,
         )
         return payment
 
