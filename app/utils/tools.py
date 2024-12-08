dfd9555b9f86d84471da6b5b0761872b90b9f0b3
Krushovice <kickstar69@yandex.ru>
2024-12-08 12:56:24 +1000

    work with user payment success

--- app/utils/tools.py
+++ app/utils/tools.py
@@ -1,5 +1,7 @@
 import logging
-
+import functools
+import asyncio
+import datetime
 
 from aiogram import Bot
 from aiogram.types import FSInputFile, Message
@@ -11,11 +13,29 @@ from app.core.config import settings
 from app.core.database import UserSchema, UserCRUD
 
 from app.keyboards import build_chat_kb
+from core.database import PaymentCRUD
 
 image_path = "app/utils/images/image3.JPEG"
+ban_image_path = "app/utils/images/ban_image.png"
 
 
-#
+def async_repeat(interval):
+    """
+    Декоратор для асинхронного вызова функции с заданным интервалом.
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs):
+            while True:
+                await func(*args, **kwargs)
+                await asyncio.sleep(interval)  # Ожидание заданного интервала
+
+        return wrapper
+
+    return decorator
+
+
 # Обработчик для получения уведомлений о статусе платежа
 async def handle_payment_notification(request, bot: Bot):
     try:
@@ -36,24 +56,22 @@ async def handle_payment_notification(request, bot: Bot):
         status = data["Status"]
         # если платеж успешен и его еще нет в бд
         if status == "CONFIRMED":
+            payment = await save_user_payment(payment_id=data["PaymentId"])
             logging.info(f"Платеж подтвержден: {data}")
-
-            # payment = save_user_payment()
-            # tg_id = payment.user.tg_id
-            # Отправка сообщения пользователю
-
-            chat_link = await bot.create_chat_invite_link(
-                chat_id=int(settings.main.channel_id),
-                name=f"Инвайт ссылка юзера: {1130398207}",
-                member_limit=1,
-            )
-            await bot.send_photo(
-                chat_id=1130398207,
-                photo=FSInputFile(image_path),
-                caption="Ваш платеж успешно подтвержден! Ссылка на закрытый чат ниже",
-                reply_markup=build_chat_kb(invite_link=chat_link.invite_link),
-            )
-            return web.json_response({"status": "ok"})
+            if payment:
+                invite_link = await work_with_chat_member(payment=payment, bot=bot)
+                await bot.send_photo(
+                    chat_id=payment.user.tg_id,
+                    photo=FSInputFile(image_path),
+                    caption="Ваш платеж успешно подтвержден! Ссылка на закрытый чат ниже",
+                    reply_markup=build_chat_kb(invite_link=invite_link),
+                )
+                return web.json_response({"status": "ok"})
+            else:
+                logging.info(f"Платеж {data["PaymentId"]} уже обработан")
+                return web.json_response(
+                    {"status": "ok", "text": "Платеж уже обработан"}
+                )
         else:
             logging.warning(f"Необработанный статус платежа: {status}")
             return web.json_response({"status": "unhandled status"})
@@ -65,7 +83,28 @@ async def handle_payment_notification(request, bot: Bot):
 
 async def save_user_payment(payment_id: int):
     # сохраняем платеж в базу данных и пробрасываем наверх
-    pass
+    check_for_exist = await PaymentCRUD.get_payment_by_pay_id(payment_id)
+
+    if check_for_exist:
+        return None
+
+    payment = await PaymentCRUD.create_payment(payment_id=payment_id)
+    return payment
+
+
+async def work_with_chat_member(payment, bot: Bot):
+    if payment.user.payments:
+        await bot.unban_chat_member(
+            chat_id=settings.main.channel_id,
+            user_id=payment.user.tg_id,
+            only_if_banned=True,
+        )
+    else:
+        chat_link = await bot.create_chat_invite_link(
+            chat_id=settings.main.channel_id,
+            name=f"Инвайт сслыка юзера: {payment.user.tg_id}",
+        )
+        return chat_link.invite_link
 
 
 async def register_user(message: Message):
@@ -82,3 +121,22 @@ async def register_user(message: Message):
         user = await UserCRUD.create_user(user=user_data)
         return user
     return user_exist
+
+
+@async_repeat(interval=24 * 3600)
+async def check_user_subs(bot: Bot):
+    users = await UserCRUD.get_users()
+    today = datetime.date.today()
+    for user in users:
+        if user.expired_at < today and not user.is_active:
+            await bot.ban_chat_member(
+                chat_id=settings.main.channel_id,
+                user_id=user.tg_id,
+                revoke_messages=True,
+            )
+            await bot.send_photo(
+                chat_id=user.tg_id,
+                photo=FSInputFile(path=ban_image_path),
+                caption=f"Ваша подписка на канал закончилась {user.expired_at}. "
+                f"Оплатите пожалуйста доступ /pay 💰",
+            )
