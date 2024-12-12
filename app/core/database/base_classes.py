e63b3949be32ea60bebe415c08c5b25920dd240d
Krushovice <kickstar69@yandex.ru>
2024-12-12 18:50:27 +1000

    few fixes in base orm classes

--- app/core/database/base_classes.py
+++ app/core/database/base_classes.py
@@ -3,6 +3,7 @@ from typing import List, Any, Dict
 from sqlalchemy import select, Result
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
 from app.core import User, Payment
 
@@ -32,6 +33,22 @@ class BaseOrm:
     @classmethod
     async def find_one_or_none(cls, session: AsyncSession, **filter_by):
         query = select(cls.model).filter_by(**filter_by)
+        result = await session.execute(query)
+        record = result.scalar_one_or_none()
+        return record
+
+    @classmethod
+    async def get_one_with_related_model(
+        cls,
+        data_id: int,
+        session: AsyncSession,
+        related_model,
+    ):
+        query = (
+            select(cls.model)
+            .where(cls.model.id == data_id)
+            .options(selectinload(related_model))
+        )
         result = await session.execute(query)
         record = result.scalar_one_or_none()
         return record
@@ -93,6 +110,28 @@ class UserOrm(BaseOrm):
         user = await cls.get_one_or_none_by_id(data_id=user_id, session=session)
         return user
 
+    @classmethod
+    async def get_user_by_tg_id(cls, session: AsyncSession, tg_id: int) -> User:
+        user = await cls.find_one_or_none(tg_id=tg_id, session=session)
+        return user
+
+    @classmethod
+    async def get_user_with_related_model_by_tg_id(
+        cls,
+        session: AsyncSession,
+        tg_id: int,
+    ) -> User:
+        query = (
+            select(User)
+            .where(User.tg_id == tg_id)
+            .options(
+                selectinload(User.payments),
+            )
+        )
+        result = await session.execute(query)
+        record = result.scalar_one_or_none()
+        return record
+
 
 class PaymentOrm(BaseOrm):
     model = Payment
@@ -101,7 +140,7 @@ class PaymentOrm(BaseOrm):
     async def add_payment(cls, session: AsyncSession, pay_data: dict) -> Payment:
         # Создаем пользователя из переданных данных
         payment = cls.model(
-            pay_id=pay_data["payment_id"],
+            pay_id=pay_data["pay_id"],
             user_id=pay_data["user_id"],
         )
         session.add(payment)
@@ -110,5 +149,25 @@ class PaymentOrm(BaseOrm):
 
     @classmethod
     async def get_payment(cls, session: AsyncSession, payment_id: int) -> Payment:
-        payment = await cls.get_one_or_none_by_id(data_id=payment_id, session=session)
+        payment = await cls.find_one_or_none(
+            pay_id=payment_id,
+            session=session,
+        )
         return payment
+
+    @classmethod
+    async def get_payment_with_related_model(
+        cls,
+        session: AsyncSession,
+        payment_id: int,
+    ) -> Payment:
+        query = (
+            select(Payment)
+            .where(Payment.pay_id == payment_id)
+            .options(
+                selectinload(Payment.user),
+            )
+        )
+        result = await session.execute(query)
+        record = result.scalar_one_or_none()
+        return record
