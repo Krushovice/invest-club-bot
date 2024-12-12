66d43f383caad09cfca9ba9f173122523caff290
Krushovice <kickstar69@yandex.ru>
2024-12-12 18:50:47 +1000

    create chat join request handler

--- app/routers/channel_handlers/admin_handlers.py
+++ app/routers/channel_handlers/admin_handlers.py
@@ -1,26 +1,86 @@
-from aiogram import Bot, Router, F
-from aiogram.types import CallbackQuery
+import logging
 
+from aiogram import Router, F
+from aiogram.types import CallbackQuery, ChatMemberUpdated, ChatJoinRequest
+
+from aiogram.exceptions import TelegramBadRequest
+from sqlalchemy.exc import SQLAlchemyError
+
+from app.keyboards.chat_kb import ChatCbData, ChatActions
 from app.core.config import settings
 
-# from app.core.models import User
-
-from app.keyboards.chat_kb import ChatCbData, ChatActions, build_chat_kb
+from app.core.database import UserCRUD
 
 router = Router(name=__name__)
 
 
 @router.callback_query(ChatCbData.filter(F.action == ChatActions.join))
-async def handle_new_member(call: CallbackQuery, bot: Bot):
-    msg = (
-        f"Приветствую!"
-        f"Добро пожаловать в узкий круг инвесторов🤝 "
-        f"Здесь вы найдёте всю актуальную информацию по сделкам и стратегии следования ✍ "
-    )
+async def handle_new_member(call: CallbackQuery):
+    await call.answer()
 
-    # if user.subscription:
-    await bot.approve_chat_join_request(
-        chat_id=int(settings.main.channel_id),
-        user_id=1130398207,
-    )
-    await call.message.answer(msg)
+
+@router.chat_join_request(F.chat.id == int(settings.main.channel_id))
+async def handle_join_request(event: ChatJoinRequest):
+    user_id = event.from_user.id
+
+    try:
+        user = await UserCRUD.get_user_by_tg_id(user_id)
+        bot = event.bot
+        if user and user.is_active:
+            await event.approve()
+            await bot.send_message(
+                chat_id=user_id,
+                text=(
+                    "Ваш запрос на вступление в канал одобрен! "
+                    "Ссылка на канал: https://t.me/+FJ3Xv4Lu5EM2ZGUy"
+                ),
+            )
+            logging.info(f"Заявка одобрена для пользователя: {event.from_user.id}")
+
+        else:
+            await event.decline()
+            await bot.send_message(
+                chat_id=user_id,
+                text="Извините, ваш запрос на вступление в канал отклонен. "
+                "Видимо проблема с оплатой 😕",
+            )
+
+    except TelegramBadRequest as e:
+        logging.error(e)
+
+    except SQLAlchemyError as e:
+        logging.error(e)
+
+    except Exception as e:
+        logging.error(e)
+
+
+@router.chat_member()
+async def on_user_join(event: ChatMemberUpdated):
+    if (
+        event.chat.id == int(settings.main.channel_id)
+        and event.new_chat_member.status == "member"
+    ):
+        # Отправляем личное сообщение
+        await event.bot.send_message(
+            event.from_user.id,  # ID пользователя, который присоединился
+            "Привет! Добро пожаловать в наш канал!",
+        )
+
+
+@router.chat_member()
+async def on_user_join_or_leave(event: ChatMemberUpdated):
+    user = event.from_user
+    chat_id = event.chat.id
+    status = event.new_chat_member.status
+
+    if status == "member":  # Пользователь присоединился
+        await event.bot.send_message(
+            chat_id, f"{user.full_name} присоединился(лась) к каналу!"
+        )
+    elif status == "left":  # Пользователь покинул канал
+        await event.bot.send_message(chat_id, f"{user.full_name} покинул(а) канал.")
+    elif status == "kicked":  # Пользователь был кикнут
+        await event.bot.send_message(
+            chat_id, f"{user.full_name} был(а) исключён(а) из канала."
+        )
