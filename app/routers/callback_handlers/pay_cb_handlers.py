9753367fe75b503715533613ff900161dc20294b
Krushovice <kickstar69@yandex.ru>
2024-12-27 16:47:21 +1000

    add help button handler

--- app/routers/callback_handlers/pay_cb_handlers.py
+++ app/routers/callback_handlers/pay_cb_handlers.py
@@ -1,13 +1,13 @@
 from aiogram import Router, F
-from aiogram.types import CallbackQuery, FSInputFile
+from aiogram.types import CallbackQuery
 
 from aiogram.utils import markdown
 
-from app.keyboards import PayCbData, PayActions, pay_kb
+from app.keyboards import PayCbData, PayActions
 
 from app.routers.commands.main_commands import command_start_handler
 
-from app.routers.channel_handlers import handle_new_member
+from app.core.config import settings
 
 
 router = Router(name=__name__)
@@ -17,3 +17,16 @@ router = Router(name=__name__)
 async def handle_back_button(call: CallbackQuery):
     await call.answer()
     await command_start_handler(message=call.message)
+
+
+@router.callback_query(PayCbData.filter(F.action == PayActions.help))
+async def handle_help_button(call: CallbackQuery):
+    await call.answer()
+    msg = (
+        f"Если у вас возникли проблемы с оплатой, доступом к каналу, "
+        f"вы можете описать суть проблемы мне на почту: {settings.main.admin_mail} и мы оперативно вам поможем👍"
+    )
+
+    await call.message.answer(
+        text=markdown.text(markdown.hbold(msg)),
+    )
